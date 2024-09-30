b8e8e9f5fdf9ee920cfdfc89622e9772325b168d
JOHNROSSDOHERTY <g00423043@atu.ie>
2024-09-30 09:43:23 +0100

    AddTwoNumbers added

--- src/main/java/ie/atu/week1/Main.java
+++ src/main/java/ie/atu/week1/Main.java
@@ -1,9 +1,24 @@
 package ie.atu.week1;
 
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    public static void main (String[] args){
+        Scanner scan1 = new Scanner(System.in);
 
+        System.out.println("Please enter first number: ");
+        double firstNum = scan1.nextDouble();
+
+        System.out.println("Please enter first number: ");
+        double secondNum = scan1.nextDouble();
+
+        double result = firstNum + secondNum;
+
+        System.out.println("The result is " +result);
+
+    }
 
 
 }
